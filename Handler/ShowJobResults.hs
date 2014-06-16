299e0ff63401cc5caec2f6ac52354b14444203f4
Stefan von der Krone <stefanvonderkrone@gmx.de>
2014-06-16 13:06:37 +0200

    fixed issue, where ShowJobResults still connects with star exec despite having all results stored into the db

--- Handler/ShowJobResults.hs
+++ Handler/ShowJobResults.hs
@@ -18,28 +18,23 @@ countResults _jobId result solverName = do
 
 getShowJobResultsR :: Int -> Handler Html
 getShowJobResultsR _jobId = do
-  con <- getConnection
-  mJobInfo <- SEC.getJobInfo con _jobId
-  case mJobInfo of
-    Nothing -> error $ "No such Job: " ++ show _jobId
-    Just jobInfo -> do
-      pJobInfos <- getJobResults _jobId
-      let jobinfos = fromPersistJobResultInfos pJobInfos
-          benchmarks = getInfo extractBenchmark jobinfos
-          solvers = getInfo extractSolver jobinfos
-          benchmarkResults = getBenchmarkResults
-                              solvers
-                              jobinfos
-                              (L.sortBy compareBenchmarks benchmarks)
-          solverNames = map snd solvers
-          countResultsByJobId = countResults _jobId
-          results = [ YES, NO, MAYBE, CERTIFIED, ERROR, OTHER ]
-      yesses <- runDB $ mapM (countResultsByJobId YES) solverNames
-      nos    <- runDB $ mapM (countResultsByJobId NO) solverNames
-      maybes <- runDB $ mapM (countResultsByJobId MAYBE) solverNames
-      certs  <- runDB $ mapM (countResultsByJobId CERTIFIED) solverNames
-      errors <- runDB $ mapM (countResultsByJobId ERROR) solverNames
-      others <- runDB $ mapM (countResultsByJobId OTHER) solverNames
-      let scores = zip results [ yesses, nos, maybes, certs, errors, others ]
-      defaultLayout $ do
-        $(widgetFile "show_job_results")
+  pJobResults <- getJobResults _jobId
+  let jobResults = fromPersistJobResultInfos pJobResults
+      benchmarks = getInfo extractBenchmark jobResults
+      solvers = getInfo extractSolver jobResults
+      benchmarkResults = getBenchmarkResults
+                          solvers
+                          jobResults
+                          (L.sortBy compareBenchmarks benchmarks)
+      solverNames = map snd solvers
+      countResultsByJobId = countResults _jobId
+      results = [ YES, NO, MAYBE, CERTIFIED, ERROR, OTHER ]
+  yesses <- runDB $ mapM (countResultsByJobId YES) solverNames
+  nos    <- runDB $ mapM (countResultsByJobId NO) solverNames
+  maybes <- runDB $ mapM (countResultsByJobId MAYBE) solverNames
+  certs  <- runDB $ mapM (countResultsByJobId CERTIFIED) solverNames
+  errors <- runDB $ mapM (countResultsByJobId ERROR) solverNames
+  others <- runDB $ mapM (countResultsByJobId OTHER) solverNames
+  let scores = zip results [ yesses, nos, maybes, certs, errors, others ]
+  defaultLayout $ do
+    $(widgetFile "show_job_results")
