0fe8227245d99f9e25bb537366dbb20afedbe065
Stefan von der Krone <stefanvonderkrone@gmx.de>
2014-07-10 10:36:23 +0200

    hopefully fixed #25 now using `TypedContent` as return-type, so that I can better specify which content to be send: html, xml, xsl

--- Handler/DisplayProof.hs
+++ Handler/DisplayProof.hs
@@ -1,8 +1,6 @@
 module Handler.DisplayProof where
 
 import Import
-import Text.Blaze
-import Data.Text.Lazy.Encoding
 import Codec.Compression.GZip
 import System.Directory
 import qualified Data.ByteString.Lazy as BSL
@@ -11,26 +9,28 @@ import qualified Data.Text.IO as TIO
 import qualified Data.Text.Read as TRead
 import Text.HTML.DOM
 import Text.XML.Cursor
-import Debug.Trace
 
-getDisplayProofR :: Text -> Handler Html
+typeXSL :: ContentType
+typeXSL = "text/xsl"
+
+getDisplayProofR :: Text -> Handler TypedContent
 getDisplayProofR path = do
   case TRead.decimal path of
     Right (_pairId, _) -> getProof _pairId
     Left _ -> getFile path
 
-getFile :: Text -> Handler Html
+getFile :: Text -> Handler TypedContent
 getFile path = do
   let filePath = "static/xsl/" ++ (T.unpack path)
   fileExists <- liftIO $ doesFileExist filePath
   if fileExists
     then do
       file <- liftIO $ TIO.readFile filePath
-      addHeader "Content-Type" "text/xsl; charset=utf-8"
-      return $ preEscapedToMarkup file
+      --addHeader "Content-Type" "text/xsl; charset=utf-8"
+      return $ toTypedContent (typeXSL, toContent file)
     else notFound
 
-getProof :: Int -> Handler Html
+getProof :: Int -> Handler TypedContent
 getProof _pairId = do
   mPersistJobPair <- runDB $ getBy $ UniqueJobPairInfo _pairId
   case mPersistJobPair of
@@ -41,17 +41,24 @@ getProof _pairId = do
         let proof = decompress $ BSL.fromStrict hp
             dom = fromDocument $ parseLBS proof
         if isHTML dom
-          then return ()
-          else addHeader "Content-Type" "text/xml; charset=utf-8"
-        return $ preEscapedToMarkup $ decodeUtf8 $ proof
-  where renderFail = defaultLayout $ do
-          [whamlet| 
-            <p>Sorry, there is no job-pair with the id #{show _pairId} or it has no html-proof!
-          |]
+          then do
+            liftIO $ putStrLn "document is html"
+            return $ toTypedContent $ (typeHtml, toContent proof)
+          else do
+            liftIO $ putStrLn "document is xml"
+            return $ toTypedContent $ repXml proof
+            --addHeader "Content-Type" "text/xml; charset=utf-8"
+        --return $ preEscapedToMarkup $ decodeUtf8 $ proof
+  where renderFail = do
+          html <- defaultLayout $ do
+                    [whamlet| 
+                      <p>Sorry, there is no job-pair with the id #{show _pairId} or it has no html-proof!
+                    |]
+          return $ toTypedContent $ (typeHtml, toContent html)
 
 isHTML :: Cursor -> Bool
 isHTML dom = let htmlRoot = element "html" dom
              in case htmlRoot of
               [] -> False
-              [e] -> True
+              _ -> True
              --trace (show htmlRoot) $ length htmlRoot > 0
