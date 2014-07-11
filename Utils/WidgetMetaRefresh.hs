d0ba6ed70034455ce494327678e8baf96b015596
Johannes Waldmann <waldmann@imn.htwk-leipzig.de>
2014-07-11 13:59:38 +0200

    remove type

--- Utils/WidgetMetaRefresh.hs
+++ Utils/WidgetMetaRefresh.hs
@@ -3,9 +3,7 @@ module Utils.WidgetMetaRefresh where
 import Import
 import Text.Blaze
 
-insertWidgetMetaRefresh ::
-  (ToWidgetHead (HandlerSite m) (t1 -> Markup), ToWidget (HandlerSite m) (t -> Markup), MonadWidget m) =>
-    m ()
+-- insertWidgetMetaRefresh ::  (ToWidgetHead (HandlerSite m) (t1 -> Markup), ToWidget (HandlerSite m) (t -> Markup), MonadWidget m) =>  m ()
 insertWidgetMetaRefresh = do
   toWidget [hamlet|
     <section>The Data is currently queried from star-exec. This site will constantly refresh until the data is fully loaded
