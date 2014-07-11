16f9a76db2ab1394132eb38169177f55ecf5da09
Stefan von der Krone <stefanvonderkrone@gmx.de>
2014-07-11 12:03:20 +0200

    added meta-widget for auto-refresh to `CompetitionWithConfig`

--- Handler/CompetitionWithConfig.hs
+++ Handler/CompetitionWithConfig.hs
@@ -3,9 +3,18 @@ module Handler.CompetitionWithConfig where
 import Import
 import StarExec.Types
 import StarExec.CompetitionResults
+import StarExec.JobData
+import Utils.WidgetMetaRefresh
 
 getCompetitionWithConfigR :: Competition -> Handler Html
 getCompetitionWithConfigR comp = do
+  let metaCats = getMetaCategories comp
+      cats = concat $ map getCategories metaCats
+      jobIds = concat $ map getJobIds cats
+  qJobs <- queryManyJobResults jobIds
   compResults <- getCompetitionResults comp
   defaultLayout $ do
+    if any (\q -> queryStatus q /= Latest) qJobs
+      then insertWidgetMetaRefresh
+      else return ()
     $(widgetFile "competition_slim")
