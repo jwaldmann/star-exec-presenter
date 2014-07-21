d123a2edd93cf6c10e046005ee9ca95a00cb7ba3
Johannes Waldmann <waldmann@imn.htwk-leipzig.de>
2014-07-21 14:06:23 +0200

    protect hidden competitions

--- Handler/Competition.hs
+++ Handler/Competition.hs
@@ -1,12 +1,19 @@
 module Handler.Competition where
 
 import Import
+import Yesod.Auth
 import StarExec.Types
 import Handler.CompetitionWithConfig
+import Data.Maybe (isJust)
+
 
 getCompetitionR :: CompetitionInfoId -> Handler Html
 getCompetitionR compId = do
   compInfo <- runDB $ get compId
   case compInfo of
-    Just ci -> getCompetitionWithConfigR $ competitionInfoCompetition ci
+    Just ci -> do
+        maid <- maybeAuthId
+        if competitionInfoPublic ci || isJust maid 
+           then getCompetitionWithConfigR $ competitionInfoCompetition ci
+           else defaultLayout [whamlet|this competition is not public, and you are not authorized to view it|]
     Nothing -> notFound
