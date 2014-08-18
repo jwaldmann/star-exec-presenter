7273e17b5b2a165d5163643354cd789693fce0c9
Stefan von der Krone <stefanvonderkrone@gmx.de>
2014-08-18 16:42:09 +0200

    temporarily deactivated `Flexible_Table`-route and `Table.(..)`-modules

--- StarExec/Concurrent.hs
+++ StarExec/Concurrent.hs
@@ -121,7 +121,7 @@ runQueryJob _jobId = do
                   liftIO $ putStrLn $ show $ map jobResultInfoStatus results
                   let processedResults =
                         if isComplexJob
-                          then unwrapResults $ getScoredResults results
+                          then (unwrapResults . getScoredResults . wrapResults) results
                           else results
 
                   runDB_exclusive $ do
