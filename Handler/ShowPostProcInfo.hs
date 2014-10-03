aa9e4fad4f401091c79141d59d59a6319143e8e7
Stefan von der Krone <stefanvonderkrone@gmx.de>
2014-10-03 14:57:09 +0200

    implemented route `ShowPostProcInfo`

--- Handler/ShowPostProcInfo.hs
+++ Handler/ShowPostProcInfo.hs
@@ -1,6 +1,14 @@
 module Handler.ShowPostProcInfo where
 
 import Import
+import Presenter.StarExec.JobData
+import Presenter.Utils.WidgetMetaRefresh
 
 getShowPostProcInfoR :: Int -> Handler Html
-getShowPostProcInfoR = error "Not yet implemented: getShowPostProcInfoR"
+getShowPostProcInfoR _procId = do
+  (QueryResult qStatus mPostProcInfo) <- queryPostProc _procId
+  defaultLayout $ do
+    case qStatus of
+      Latest -> return ()
+      Pending _ -> insertWidgetMetaRefresh
+    $(widgetFile "show_post_proc_info")
