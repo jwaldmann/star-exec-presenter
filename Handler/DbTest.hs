285333f7230f8f7a47c3841723fe3bc880f6795f
rm-- <r.muhl@gmx.net>
2015-07-09 18:35:53 +0200

    Added first check of cpuTime attribute

--- Handler/DbTest.hs
+++ Handler/DbTest.hs
@@ -2,15 +2,33 @@ module Handler.DbTest where
 
 import Import
 import Presenter.PersistHelper
+import Presenter.Model.Entities()
 
+-- slowCpuTimeLimit :: Int
+slowCpuTimeLimit = 10
 
 getDbTestR :: JobID -> Handler Html
 getDbTestR jid = do
   jobResults <- getPersistJobResults jid
-  let n = length jobResults
+ 
+  let concepts = createConcepts  $ getStarExecResults jobResults
+  let n = length $  jobResults
   defaultLayout [whamlet|
+    #{show concepts}
     #{show n}
     <ul>
-    $forall i <- jobResults
-      <li> #{show i}
+    $forall jobResult <- getStarExecResults jobResults
+      <li> #{show jobResult}
+      <li> #{show $ jobResultInfoResult jobResult}
+      <li> #{show $ jobResultInfoSolver jobResult}
+      <li> #{show $ jobResultInfoCpuTime jobResult}
+      <li> #{show $ jobResultInfoWallclockTime jobResult}
     |]
+
+
+
+createConcepts :: [JobResultInfo] -> [Bool]
+createConcepts = map (isFast . jobResultInfoCpuTime)
+
+isFast :: (Num a, Ord a) => a -> Bool
+isFast a = a < slowCpuTimeLimit
