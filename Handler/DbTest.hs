e9147ab8461428f1b1509ffeceaab67b6b3a6b95
rm-- <r.muhl@gmx.net>
2015-07-14 21:55:34 +0200

    Added function to collect all attributes
    There is also now a function to take these attributes and save them in a Map.
    The jobPairId is used as the key which maps to the corresponding attributes.

--- Handler/DbTest.hs
+++ Handler/DbTest.hs
@@ -1,34 +1,68 @@
 module Handler.DbTest where
 
 import Import
+import Data.List
+import           Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+-- import           Data.Set (Set)
+-- import qualified Data.Set as Set
 import Presenter.PersistHelper
 import Presenter.Model.Entities()
 
--- slowCpuTimeLimit :: Int
-slowCpuTimeLimit = 10
+
+slowCpuTimeLimit :: ((Num Double, Ord Double)) => Double
+slowCpuTimeLimit = 10.0
 
 getDbTestR :: JobID -> Handler Html
 getDbTestR jid = do
   jobResults <- getPersistJobResults jid
- 
-  let concepts = createConcepts  $ getStarExecResults jobResults
-  let n = length $  jobResults
+  let jobResultInfos = createObjectAttributeRelations $ getStarExecResults jobResults
+  let x = Map.toList jobResultInfos
+  let xn = length $ x
   defaultLayout [whamlet|
-    #{show concepts}
-    #{show n}
+    #{show xn}
     <ul>
-    $forall jobResult <- getStarExecResults jobResults
-      <li> #{show jobResult}
-      <li> #{show $ jobResultInfoResult jobResult}
-      <li> #{show $ jobResultInfoSolver jobResult}
-      <li> #{show $ jobResultInfoCpuTime jobResult}
-      <li> #{show $ jobResultInfoWallclockTime jobResult}
+    $forall (jobPairId, jobPairAttributes) <- Map.toList jobResultInfos
+      <li> #{show jobPairId}: #{show jobPairAttributes}
     |]
+    -- <ul>
+    -- $forall jobResult <- getStarExecResults jobResults
+    --   <li> #{show jobResult}
+
+
+createConcept :: Map Int (Int, Text, Bool, SolverResult) -> [(a,b)]
+createConcept = undefined
+
+
+createObjectAttributeRelations :: [JobResultInfo] -> Map Int (Int, Text, Bool, SolverResult)
+createObjectAttributeRelations jobResults = do
+  let jobResultInfoPairIds = map (jobResultInfoPairId) jobResults
+  let attrs = getAttributeCollection jobResults
+  let objAttrRel = zip jobResultInfoPairIds attrs
+  Map.fromList objAttrRel
+
+
+getAttributeCollection :: [JobResultInfo] -> [(Int, Text, Bool, SolverResult)]
+getAttributeCollection jobResults = do
+  let cpuTimeEvaluations = evaluateCpuTime jobResults
+  let jobResultInfoSolvers = map (jobResultInfoSolver) jobResults
+  let jobResultInfoResults = map (jobResultInfoResult) jobResults
+  let jobResultInfoBenchmarkIds = map (jobResultInfoBenchmarkId) jobResults
+  zip4 jobResultInfoBenchmarkIds jobResultInfoSolvers cpuTimeEvaluations jobResultInfoResults
+
+
+evaluateCpuTime :: [JobResultInfo] -> [Bool]
+evaluateCpuTime = map (isLow . jobResultInfoCpuTime)
+
+isLow :: (Num Double, Ord Double) => Double -> Bool
+isLow a = a > slowCpuTimeLimit
+
+-- evaluateResult :: [JobResultInfo] -> [SolverResult]
+-- evaluateResult = map (jobResultInfoResult)
+
+-- copied from: http://rosettacode.org/wiki/Power_set#Haskell
+powerset :: Foldable t => t a -> [[a]]
+powerset = foldr (\x acc -> acc ++ map (x:) acc) [[]]
 
 
 
-createConcepts :: [JobResultInfo] -> [Bool]
-createConcepts = map (isFast . jobResultInfoCpuTime)
-
-isFast :: (Num a, Ord a) => a -> Bool
-isFast a = a < slowCpuTimeLimit
