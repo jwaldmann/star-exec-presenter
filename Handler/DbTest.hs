ec2c8209a5c8c89af786ce33ad2b1be053e30d79
rm-- <r.muhl@gmx.net>
2015-07-17 18:10:14 +0200

    Added new datatype for key of attribute-object-relation
    Change value of object-attribute-relation-map to record.  Also removed powerset functions
    and its usage. Now there is a limit of hard coded limit for take subsets.

--- Handler/DbTest.hs
+++ Handler/DbTest.hs
@@ -2,13 +2,23 @@ module Handler.DbTest where
 
 import Import
 import Data.List
-import Data.Maybe
 import           Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
 -- import           Data.Set (Set)
 -- import qualified Data.Set as Set
 import Presenter.PersistHelper
 import Presenter.Model.Entities()
+
+data Attribute = ASolverName Text | ASlowCpuTime Bool | ASolverResult SolverResult
+ deriving (Eq, Ord, Show)
+
+
+data JobPairAttributes = JobPairAttributes
+  { benchmarkId :: Int
+  , solverName  :: Text
+  , slowCpuTime :: Bool
+  , solverResult :: SolverResult
+  } deriving (Show)
 
 
 -- all job pairs with a response time greater 10 seconds is slow
@@ -18,83 +28,70 @@ slowCpuTimeLimit = 10.0
 getDbTestR :: JobID -> Handler Html
 getDbTestR jid = do
   jobResults <- getPersistJobResults jid
-  let jobResultInfos = createObjectAttributeRelations $ getStarExecResults jobResults
-  let concepts = createConcepts jobResultInfos
+  let objAttrRel = createObjectAttributeRelation $ getStarExecResults jobResults
+  let attrObjRel = createAttributeObjectReleation objAttrRel
+  let concepts = createConcepts objAttrRel attrObjRel
 
-  let x = Map.toList jobResultInfos
+  let x = Map.toList objAttrRel
   let xn = length $ x
   defaultLayout [whamlet|
     #{show xn}
     #{show $ length concepts}
+    #{show concepts}
+    <h1>Objects an its attributes
     <ul>
-    $forall (jobPairId, jobPairAttributes) <- Map.toList jobResultInfos
+    $forall (jobPairId, jobPairAttributes) <- Map.toList objAttrRel
       <li> #{show jobPairId}: #{show jobPairAttributes}
+    <h1>Attributes with its objects
+    <ul>
+    $forall (attr, objects) <- Map.toList attrObjRel
+      <li> #{show attr}: #{show objects}
     |]
     -- <ul>
     -- $forall jobResult <- getStarExecResults jobResults
     --   <li> #{show jobResult}
 
 
-createConcepts :: Map Int (Int, Text, Bool, SolverResult) -> [[Int]]
-createConcepts jobResultInfos = do
-  let keys = getPowerset $ Map.keys jobResultInfos
-  if isJust keys
-    then concat $ maybeToList keys
-    else [[42::Int]]
-  -- let concepts = 
-  -- [(,)]
+createConcepts :: Map Int JobPairAttributes -> Map Attribute [Int] -> [[Int]]
+createConcepts objAttrRel attrsObjRel = do
+  take 20 $ subsequences $ Map.keys objAttrRel
 
 
-getPowerset :: [Int] -> Maybe [[Int]]
-getPowerset set =
-  if isSubseqToBig set
-    then Nothing
-    else Just $ subsequences set
-
-
--- condition to do not calculate powersets of a set with more than 26 elements
-isSubseqToBig :: [Int] -> Bool
-isSubseqToBig l =
-  if length l > 27
-    then True
-    else False
-
-
-createObjectAttributeRelations :: [JobResultInfo] -> Map Int (Int, Text, Bool, SolverResult)
-createObjectAttributeRelations jobResults = do
+createObjectAttributeRelation :: [JobResultInfo] -> Map Int JobPairAttributes
+createObjectAttributeRelation jobResults = do
   let jobResultInfoPairIds = map (jobResultInfoPairId) jobResults
   let attrs = getAttributeCollection jobResults
   let objAttrRel = zip jobResultInfoPairIds attrs
   Map.fromList objAttrRel
 
 
-getAttributeCollection :: [JobResultInfo] -> [(Int, Text, Bool, SolverResult)]
-getAttributeCollection jobResults = do
-  let cpuTimeEvaluations = evaluateCpuTime jobResults
-  let jobResultInfoSolvers = map (jobResultInfoSolver) jobResults
-  let jobResultInfoResults = map (jobResultInfoResult) jobResults
-  let jobResultInfoBenchmarkIds = map (jobResultInfoBenchmarkId) jobResults
-  zip4 jobResultInfoBenchmarkIds jobResultInfoSolvers cpuTimeEvaluations jobResultInfoResults
+createAttributeObjectReleation :: Map Int JobPairAttributes -> Map Attribute [Int]
+createAttributeObjectReleation objAttrRel = do
+  let objAttrs = Map.toList objAttrRel
+  -- very ugly and to explicit!
+  let cpuTimes = map (\(a,b) -> (a, slowCpuTime b)) objAttrs
+  let slowCpuTimes = map (\(a,_) -> a) $ filter (\(_,b) -> b == True) cpuTimes
+  let fastCpuTimes = map (\(a,_) -> a) $ filter (\(_,b) -> b == False) cpuTimes
+  
+  let solverResults = map (\(a,b) -> (a, solverResult b)) objAttrs
+  let maybeSolverResults = map (\(a,_) -> a) $ filter (\(_,b) -> b == MAYBE) solverResults
+  -- let otherSolverResults = map (\(a,_) -> a) $ filter (\(_,b) -> b == OTHER) solverResults
 
+  let attrsObjRel = Map.insert (ASlowCpuTime True) slowCpuTimes Map.empty
+  let attrsObjRels = Map.insert (ASlowCpuTime False) fastCpuTimes attrsObjRel
+  Map.insert (ASolverResult MAYBE) maybeSolverResults attrsObjRels
+
+
+getAttributeCollection :: [JobResultInfo] -> [JobPairAttributes]
+getAttributeCollection jobResults = do
+  -- would it be better to remove some attributes of the existing record?
+  let jobResultInfoBenchmarkIds = map (jobResultInfoBenchmarkId) jobResults
+  let jobResultInfoSolvers = map (jobResultInfoSolver) jobResults
+  let cpuTimeEvaluations = evaluateCpuTime jobResults
+  let jobResultInfoResults = map (jobResultInfoResult) jobResults
+  let attrs = zip4 jobResultInfoBenchmarkIds jobResultInfoSolvers cpuTimeEvaluations jobResultInfoResults
+  map (\(a,b,c,d)-> JobPairAttributes {benchmarkId = a, solverName = b, slowCpuTime = c, solverResult = d}) attrs
 
 evaluateCpuTime :: [JobResultInfo] -> [Bool]
-evaluateCpuTime = map (isLow . jobResultInfoCpuTime)
+evaluateCpuTime = map ((\t -> t > slowCpuTimeLimit ). jobResultInfoCpuTime)
 
-isLow :: (Num Double, Ord Double) => Double -> Bool
-isLow a = a > slowCpuTimeLimit
-
--- evaluateResult :: [JobResultInfo] -> [SolverResult]
--- evaluateResult = map (jobResultInfoResult)
-
--- copied from: http://rosettacode.org/wiki/Power_set#Haskell
--- powerset :: Foldable t => t a -> [[a]]
--- powerset = foldr (\x acc -> acc ++ map (x:) acc) [[]]
-
-
--- rfilterM :: Monad m => (a -> m Bool) -> [a] -> m [a]
--- rfilterM f []     = return []
--- rfilterM f (x:xs) = do mxs <- rfilterM f xs; pred <- f x
---                        if pred then return (x:mxs)
---                                else return mxs
--- 
--- powerset' = rfilterM (const [True,False])
