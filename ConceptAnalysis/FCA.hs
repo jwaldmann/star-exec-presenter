a4fb87b3ac8e563054ea7586fa4fdee7e06d97aa
René Muhl <r.muhl@gmx.net>
2015-07-23 14:32:10 +0200

    Moved formal concept analysis to seperate module

---
+++ ConceptAnalysis/FCA.hs
@@ -0,0 +1,37 @@
+module ConceptAnalysis.FCA where
+
+import Import
+import           Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+import           Data.Set (Set)
+import qualified Data.Set as Set
+
+-- contextFromList [(1,["foo", "bar"]), (2, ["foo","baz"])]
+-- getAttributes c $ Set.fromList [1,2]
+-- getObjects c $ Set.fromList ["foo"]
+
+
+data Context ob at = Context
+  { fore :: Map ob (Set at)
+    ,back :: Map at (Set ob)
+  } deriving (Show)
+
+contextFromList :: (Ord ob, Ord at) => [(ob, [at])] -> Context ob at
+contextFromList l = Context 
+  { fore=Map.fromListWith Set.union $ map (\(ob, ats) -> (ob, Set.fromList ats)) l
+  , back=Map.fromListWith Set.union $ do (ob, ats) <- l; at <- ats; return (at,Set.singleton ob)
+  }
+
+attributes :: (Ord ob, Ord at) => Context ob at -> Set at
+attributes c = Map.keysSet (back c)
+
+getAttributes :: (Ord ob, Ord at) => Context ob at -> Set ob -> Set at
+getAttributes c obs = foldr Set.intersection (attributes c)
+  $ map (\o -> fore c Map.! o) $ Set.toList obs
+
+objects :: (Ord ob, Ord at) => Context ob at -> Set ob
+objects c = Map.keysSet (fore c)
+
+getObjects :: (Ord ob, Ord at) => Context ob at -> Set at -> Set ob
+getObjects c ats = foldr Set.intersection (objects c)
+  $ map (\a -> back c Map.! a) $ Set.toList ats
--- Handler/DbTest.hs
+++ Handler/DbTest.hs
@@ -9,10 +9,8 @@ import           Data.Set (Set)
 import qualified Data.Set as Set
 import Presenter.PersistHelper
 import Presenter.Model.Entities()
+import ConceptAnalysis.FCA
 
--- let c = contextFromList [(1,["foo", "bar"]), (2, ["foo"])]
--- getAttributes c $ Set.fromList [1,2]
--- getObjects c $ Set.fromList ["foo"]
 
 type JobPairId = Int
 
@@ -29,31 +27,6 @@ data JobPairAttributes = JobPairAttributes
   , slowCpuTime :: Bool
   , solverResult :: SolverResult
   } deriving (Show)
-
-data Context ob at = Context
-  { fore :: Map ob (Set at)
-    ,back :: Map at (Set ob)
-  } deriving (Show)
-
-contextFromList :: (Ord ob, Ord at) => [(ob, [at])] -> Context ob at
-contextFromList l = Context 
-  { fore=Map.fromListWith Set.union $ map (\(ob, ats) -> (ob, Set.fromList ats)) l
-  , back=Map.fromListWith Set.union $ do (ob, ats) <- l; at <- ats; return (at,Set.singleton ob)
-  }
-
-attributes :: (Ord ob, Ord at) => Context ob at -> Set at
-attributes c = Map.keysSet (back c)
-
-getAttributes :: (Ord ob, Ord at) => Context ob at -> Set ob -> Set at
-getAttributes c obs = foldr Set.intersection (attributes c)
-  $ map (\o -> fore c Map.! o) $ Set.toList obs
-
-objects :: (Ord ob, Ord at) => Context ob at -> Set ob
-objects c = Map.keysSet (fore c)
-
-getObjects :: (Ord ob, Ord at) => Context ob at -> Set at -> Set ob
-getObjects c ats = foldr Set.intersection (objects c)
-  $ map (\a -> back c Map.! a) $ Set.toList ats
 
 
 -- all job pairs with a response time greater 30 seconds is slow
