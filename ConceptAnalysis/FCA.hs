94205e23ae47f669740f5a14c7e692f62871d1d2
rm-- <r.muhl@gmx.net>
2015-07-24 00:51:05 +0200

    Added function for concept creation

--- ConceptAnalysis/FCA.hs
+++ ConceptAnalysis/FCA.hs
@@ -1,6 +1,7 @@
 module ConceptAnalysis.FCA where
 
 import Import
+import Data.List
 import           Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
 import           Data.Set (Set)
@@ -24,15 +25,20 @@ contextFromList l = Context
   }
 
 attributes :: (Ord ob, Ord at) => Context ob at -> Set at
-attributes c = Map.keysSet (back c)
+attributes c = Map.keysSet $ back c
 
 getAttributes :: (Ord ob, Ord at) => Context ob at -> Set ob -> Set at
 getAttributes c obs = foldr Set.intersection (attributes c)
   $ map (\o -> fore c Map.! o) $ Set.toList obs
 
 objects :: (Ord ob, Ord at) => Context ob at -> Set ob
-objects c = Map.keysSet (fore c)
+objects c = Map.keysSet $ fore c
 
 getObjects :: (Ord ob, Ord at) => Context ob at -> Set at -> Set ob
 getObjects c ats = foldr Set.intersection (objects c)
   $ map (\a -> back c Map.! a) $ Set.toList ats
+
+concepts :: (Ord ob, Ord at) => Context ob at-> [([ob], [at])]
+concepts c = do
+  let atsPs = map (\ats -> Set.fromList ats) $ subsequences $ Set.toList $ attributes c
+  [(Set.toList $ getObjects c ats, Set.toList ats) | ats <- atsPs, ats == (getAttributes c $ getObjects c ats)]
