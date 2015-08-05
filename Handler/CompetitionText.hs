a46cccba78e3691fdacf8f6f4ec413cdc9e8225b
Johannes Waldmann <waldmann@imn.htwk-leipzig.de>
2015-08-05 17:41:48 +0200

    add update method

--- Handler/CompetitionText.hs
+++ Handler/CompetitionText.hs
@@ -5,9 +5,18 @@ import Yesod.Auth
 import Data.Maybe
 import qualified Data.Text as T
 import Presenter.Output
+import Data.String
 
 -- inputForm :: Maybe Text -> MForm Handler (FormResult Textarea, Widget)
-inputForm mcomp = renderDivs $ areq textareaField "input" ( fmap Textarea mcomp )
+inputForm mcomp =
+  let fs s = FieldSettings
+        { fsLabel = fromString s
+        , fsTooltip = Nothing
+        , fsId = Nothing
+        , fsName = Nothing
+        , fsAttrs = [ ("cols", "80"), ("rows", "20") ]
+        } 
+  in renderDivs $ areq textareaField (fs "input") ( fmap Textarea mcomp )
 
 getCompetitionTextR :: CompetitionInfoId -> Handler Html
 getCompetitionTextR compId = do
@@ -26,11 +35,13 @@ getCompetitionTextR compId = do
 postCompetitionTextR :: CompetitionInfoId -> Handler Html
 postCompetitionTextR compId = do
   Just aid <- maybeAuthId
+  Just cinfo <- runDB $ get compId
   ((result, widget), enctype) <- runFormPost $ inputForm Nothing
   case result of
     FormSuccess t -> do
       case readsPrec 0 $ T.unpack  $ unTextarea t :: [(Competition,String)] of
         [ (comp',"") ] -> do
+          runDB $ replace compId $ cinfo { competitionInfoCompetition = comp' }
           defaultLayout [whamlet|
             <p> #{show comp'}
             |]
