e53660b1588e0464f3ff264c4d132b4f592a2037
René Muhl <rmuhl@mailbox.org>
2015-11-08 23:02:50 +0100

    Played with some nodes and edges

--- ConceptAnalysis/DotGraph.hs
+++ ConceptAnalysis/DotGraph.hs
@@ -18,7 +18,7 @@ dotted_graph = TL.unpack $ renderDot $ toDot $ graphToDot graph_params $ graph
 -- graphToDot :: (Ord cl, Graph gr) => GraphvizParams Node nl el cl l -> gr nl el -> DotGraph Node
 
 graph :: Gr TL.Text TL.Text
-graph = mkGraph [ (2,"one"), (4,"three"), (5,"") ] [ (2,4,"edge label") ]
+graph = mkGraph [ (2,"one"), (4,"three"), (3,""), (5,"") ] [ (5,4,""), (2,4,"edge label"), (5,2,""), (2,2,""),  (3,2,"") ]
 -- mkGraph :: [LNode a] -> [LEdge b] -> gr a b
 
 graph_params :: GraphvizParams n TL.Text TL.Text () TL.Text
