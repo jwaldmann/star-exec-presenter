8368a23e9b799be256ffd335682c5a74ad5e1686
René Muhl <rmuhl@mailbox.org>
2015-11-22 19:40:21 +0100

    Corrected test and test settings

--- tests/HomeTest.hs
+++ tests/HomeTest.hs
@@ -13,20 +13,7 @@ homeSpecs =
         yit "loads the index and checks it looks right" $ do
             get HomeR
             statusIs 200
-            htmlAllContain "h1" "Hello"
-
-            request $ do
-                setMethod "POST"
-                setUrl HomeR
-                addToken
-                fileByLabel "Choose a file" "tests/main.hs" "text/plain" -- talk about self-reference
-                byLabel "What's on the file?" "Some Content"
-
-            statusIs 200
-            printBody
-            htmlCount ".message" 1
-            htmlAllContain ".message" "Some Content"
-            htmlAllContain ".message" "text/plain"
+            htmlAllContain "h1" "Termination Competitions"
 
         -- This is a simple example of using a database access in a test.  The
         -- test will succeed for a fresh scaffolded site with an empty database,
