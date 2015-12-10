8a55e64a4f899bce2a08314cd9a42619598f7036
René Muhl <rmuhl@mailbox.org>
2015-12-10 01:36:56 +0100

    Added job fetch from starexec for concepts route

--- Handler/Concepts.hs
+++ Handler/Concepts.hs
@@ -4,18 +4,25 @@ import Import
 import Data.List (elemIndex)
 import qualified Data.Set as Set
 import Data.Maybe
+import Control.Monad (when)
 import Presenter.PersistHelper
 import Presenter.Model.Entities()
+import Presenter.StarExec.JobData (queryJob)
+import Presenter.Utils.WidgetMetaRefresh (insertWidgetMetaRefresh)
 import ConceptAnalysis.FCA
 import ConceptAnalysis.FCAPreparation
 
 -- route to show concepts of given JobID
 getConceptsR :: JobID -> Handler Html
 getConceptsR jid = do
+  QueryResult qStatus _ <- queryJob jid
   jobResults <- getPersistJobResults jid
   let contextData = collectData $ getStarExecResults jobResults
   let context = contextFromList contextData
   let concepts' = concepts context
   defaultLayout $ do
+    -- fetch job from starexec if not present in database
+    when (qStatus /= Latest)
+     insertWidgetMetaRefresh
     setTitle "concepts"
     $(widgetFile "concepts")
