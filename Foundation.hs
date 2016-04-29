454bf03efe70909dc3ea74a7564e971d93a33af0
René Muhl <rmuhl@mailbox.org>
2016-04-29 23:51:27 +0200

    Upgraded bootstrap, removed normalize and jquery
    * boostrap from v3.1.1 to v3.3.6
    * normalize.css is a part of bootstrap as you can see here:
    https://github.com/twbs/bootstrap/blob/master/less/normalize.less
    * jquery isn't used anywhere.

--- Foundation.hs
+++ Foundation.hs
@@ -110,13 +110,9 @@ instance Yesod App where
 
         pc <- widgetToPageContent $ do
             $(combineStylesheets 'StaticR
-                [ css_normalize_css
-                , css_bootstrap_css
-                ])
+                [ css_bootstrap_css ])
             $(combineScripts 'StaticR
-                [ js_jquery_js
-                , js_bootstrap_js
-                ])
+                [ js_bootstrap_js ])
             $(widgetFile "default-layout")
         withUrlRenderer $(hamletFile "templates/default-layout-wrapper.hamlet")
 
