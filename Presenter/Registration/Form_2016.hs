710887bca5a6662e26de04035f178dfb357fb652
Johannes Waldmann <johannes.waldmann@htwk-leipzig.de>
2016-08-30 17:44:16 +0200

    update matchbox registration

--- Presenter/Registration/Form_2016.hs
+++ Presenter/Registration/Form_2016.hs
@@ -308,9 +308,9 @@ tc_matchbox :: Competition [Participant]
 tc_matchbox = Competition "Termination Competition 2016"
      [MetaCategory "Termination of Term Rewriting (and Transition Systems)"
           [Category "SRS Standard"
-               [Participant {participantName = "matchbox" , solver_config = Just (62205, 9251,165349)}]
+               [Participant {participantName = "matchbox" , solver_config = Just (62205, 9303,165453)}]
           ,Category "Cycles"
-               [Participant {participantName = "matchbox" , solver_config = Just (62205, 9251,165350)}]
+               [Participant {participantName = "matchbox" , solver_config = Just (62205, 9303,165454)}]
           ]
      ]
 
