d079865e0c150c54e8f620b42ab0d696904f91f8
Johannes Waldmann <johannes.waldmann@htwk-leipzig.de>
2016-08-30 17:45:27 +0200

    add fork text

--- Handler/Participant.hs
+++ Handler/Participant.hs
@@ -8,8 +8,15 @@ getParticipantR year name = do
   defaultLayout $ do
     [whamlet|
      <h1>Registration Template in #{show year} for #{name} 
-     if you want to update your submission, send email in exactly this form:
+     if you want to update your submission,
+     <p>
+     send email in exactly this form:
      <pre>#{show $ extract year name}
+     <em>or
+     fork <a href="https://github.com/jwaldmann/star-exec-presenter">star-exec-presenter</a>,
+     edit <tt>Presenter/Registration/Form_2016.hs</tt>,
+     and send a pull request.
+     <p>
      solver_config must be Just (sp,so,co) where
      <ul>
         <li>sp = starexec id of space that contains the solver
