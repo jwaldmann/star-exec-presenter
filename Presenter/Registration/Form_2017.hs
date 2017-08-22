4c583b83229ef09bc9bc984c935e12ad4aae35a2
Johannes Waldmann <johannes.waldmann@htwk-leipzig.de>
2017-08-22 14:17:15 +0200

    update SOL

--- Presenter/Registration/Form_2017.hs
+++ Presenter/Registration/Form_2017.hs
@@ -357,4 +357,4 @@ tc_sol = Competition "Termination Competition 2017"
  [MetaCategory "Termination of Term Rewriting (and Transition Systems)"
    [Category "Higher-Order rewriting (union beta)"
       [Participant {participantName = "SOL" ,
-                    solver_config = Just (277287,12905,228005)}]]]
+                    solver_config = Just (277287,12945,228449)}]]]
