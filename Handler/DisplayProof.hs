00e9adf86788a961c73a5e8cf7fe49a49db5b82a
Johannes Waldmann <johannes.waldmann@htwk-leipzig.de>
2017-09-02 19:02:03 +0200

    repair processing instruction for cpf

--- Handler/DisplayProof.hs
+++ Handler/DisplayProof.hs
@@ -7,8 +7,12 @@ import qualified Data.ByteString.Lazy as BSL
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 import qualified Data.Text.Read as TRead
-import Text.HTML.DOM
+import Text.HTML.DOM (parseLBS)
 import Text.XML.Cursor
+import Text.XML (Document(..), Instruction(..), Miscellaneous(..), Prologue(..)
+                , parseLBS, renderLBS)
+import Control.Monad.Logger
+import Data.Default.Class (def)
 
 typeXSL :: ContentType
 typeXSL = "text/xsl"
@@ -33,19 +37,39 @@ getProof :: Int -> Handler TypedContent
 getProof _pairId = do
   mPersistJobPair <- runDB $ getBy $ UniqueJobPairInfo _pairId
   case mPersistJobPair of
-    Nothing -> renderFail
+    Nothing -> renderFail _pairId "no such job pair"
     Just jp -> case jobPairInfoHtmlProof $ entityVal jp of
-      Nothing -> renderFail
+      Nothing -> renderFail _pairId "job pair output has no html/xml contents"
       Just hp -> do
         let proof = decompress $ BSL.fromStrict hp
-            dom = fromDocument $ parseLBS proof
-        if isHTML dom
-          then return $ toTypedContent $ (typeHtml, toContent proof)
-          else return $ toTypedContent $ repXml proof
-  where renderFail = do
+        case  Text.XML.parseLBS def proof of
+          Right xml_doc -> do
+            logWarnN $ T.pack $ "------------" ++ show ( documentPrologue xml_doc )
+            let xml_doc' = repair_stylesheet xml_doc
+            return $ toTypedContent ( typeXml, toContent $ repXml $ renderLBS def xml_doc )
+          Left exc -> do
+            let html_doc = Text.HTML.DOM.parseLBS proof
+                cursor = fromDocument html_doc
+            if isHTML cursor
+              then return $ toTypedContent $ (typeHtml, toContent proof)
+              else renderFail _pairId "job pair output has invalid html contents"
+
+cpfHTML_prologue = Prologue
+    { prologueBefore =
+        [MiscInstruction
+         (Instruction { instructionTarget = "xml-stylesheet"
+                      , instructionData = "type=\"text/xsl\" href=\"cpfHTML.xsl\""})
+        ]
+    , prologueDoctype = Nothing, prologueAfter = []
+    }
+
+repair_stylesheet doc = doc { documentPrologue = cpfHTML_prologue }
+
+renderFail :: Int -> Text -> Handler TypedContent
+renderFail _pairId msg = do
           html <- defaultLayout $ do
                     [whamlet| 
-                      <p>Sorry, there is no job-pair with the id #{show _pairId} or it has no html-proof!
+                      <p>job pair #{show _pairId}: #{msg}
                     |]
           return $ toTypedContent $ (typeHtml, toContent html)
 
