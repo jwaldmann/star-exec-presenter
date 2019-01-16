22d8202aa60a554874c232c9f4ce71fc5d5e0b73
Johannes Waldmann <johannes.waldmann@htwk-leipzig.de>
2019-01-16 03:46:45 +0100

    change settings

--- keter.sh
+++ keter.sh
@@ -5,14 +5,14 @@ ARCH=x86_64-linux
 # if you change this, be sure to change stack.yaml and config/keter.yml as well
 # RESOLVER=lts-8.22
 # GHC=8.0.2
-RESOLVER=nightly-2017-08-15
-GHC=8.2.1
+RESOLVER=lts-13.3
+GHC=8.6.3
 
 APP=.stack-work/install/$ARCH/$RESOLVER/$GHC/bin/star-exec-presenter
 KET=star-exec-presenter.keter
 
 strip $APP
 rm -fv $KET
-tar -c --dereference --hard-dereference -z -v -f $KET $APP static config TPDB-*_XML.zip mario_wenzel_XML.zip 
+tar -c --dereference --hard-dereference -z -v -f $KET $APP static config TPDB-*_XML.zip johannes_waldmann_tpdb-8.0.7_XML.zip  mario_wenzel_XML.zip 
 # scp $KET termcomp.imn.htwk-leipzig.de:/opt/keter/incoming
 cp -v $KET /opt/keter/incoming
