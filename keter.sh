e7a207d991cc3ff43e89b24a6bb59ee362b66816
Johannes Waldmann <johannes.waldmann@htwk-leipzig.de>
2020-07-02 19:50:51 +0200

    update keter info

--- keter.sh
+++ keter.sh
@@ -3,16 +3,16 @@
 ARCH=x86_64-linux
 
 # if you change this, be sure to change stack.yaml and config/keter.yml as well
-# RESOLVER=lts-8.22
-# GHC=8.0.2
-RESOLVER=lts-13.9
-GHC=8.6.3
 
-APP=.stack-work/install/$ARCH/$RESOLVER/$GHC/bin/star-exec-presenter
+# RESOLVER=lts-13.16
+# GHC=8.6.4
+
+APP=star-exec-presenter
 KET=star-exec-presenter.keter
 
-strip $APP
+cp -v $(which $APP) bin/$APP
+strip bin/$APP
 rm -fv $KET
-tar -c --dereference --hard-dereference -z -v -f $KET $APP static config TPDB-*_XML.zip johannes_waldmann_tpdb-8.0.7_XML.zip  mario_wenzel_XML.zip 
+tar -c --dereference --hard-dereference -z -v -f $KET bin/$APP static config TPDB-*_XML.zip johannes_waldmann_tpdb-8.0.7_XML.zip  mario_wenzel_XML.zip termcomp2019_XML.zip termcomp2020_XML.zip
 # scp $KET termcomp.imn.htwk-leipzig.de:/opt/keter/incoming
 cp -v $KET /opt/keter/incoming
