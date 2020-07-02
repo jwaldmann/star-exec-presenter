d15856630049cb3f87ade2d5ec7ca1b6ec23bf60
Johannes Waldmann <johannes.waldmann@htwk-leipzig.de>
2020-07-02 19:31:02 +0200

    add info on benchmark ids for 2020 competition

--- src/Application.hs
+++ src/Application.hs
@@ -160,6 +160,7 @@ makeFoundation conf = do
       , "TPDB-10.6a_XML.zip"
       , "TPDB-11_XML.zip"
       , "termcomp2019_XML.zip" -- contains new benchmarks uploaded by Akihisa, used in competition
+      , "termcomp2020_XML.zip"
       ]
 
     let logger = Yesod.Core.Types.Logger loggerSet' getter
